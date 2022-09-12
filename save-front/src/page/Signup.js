2919d7e75f3ebe081a2ab5e1c4b193b10414c9af
Kimseonghyoek <cilo1223@naver.com>
2022-09-13 02:47:17 +0900

    Update Signup Page Design

--- save-front/src/page/Signup.js
+++ save-front/src/page/Signup.js
@@ -6,10 +6,19 @@ import "./css/signup.css";
 
 const Singup = (props) => {
   return (
-    <div id="background" className="flex justify-center items-center">
-      <Card styled="bg-maincolor2 flex flex-col w-signup h-signup  p-4 justify-center">
-        <h1 className="text-white text-middle2 p-4 pb-2">SAVE BOOK 에 오신 것을 환영합니다.</h1>
-        <Link to="/"><p className="inline-block text-white p-4 pt-1">혹시 이미 가입하셨나요?</p></Link>
+    <div id="background" className="flex justify-center items-center flex-col">
+      <h1 className="text-white text-big text-center p-3 font-Chewy">
+        Save Book
+      </h1>
+      <Card styled="bg-maincolor2 flex flex-col  h-signup  p-4 justify-center">
+        <h1 className="text-white text-middle2 p-4 pb-2">
+          SAVE BOOK 에 오신 것을 환영합니다.
+        </h1>
+        <Link to="/">
+          <p className="inline-block text-maincolor1 text-small p-4 pt-1">
+            혹시 이미 가입하셨나요?
+          </p>
+        </Link>
         <LoginForm />
       </Card>
     </div>

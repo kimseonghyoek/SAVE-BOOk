62372c991caf6d24117b393e1c232c398c1e91d4
Kimseonghyoek <cilo1223@naver.com>
2022-09-12 20:13:18 +0900

    Update Backend

--- pc-back/DB/mysql.js
+++ pc-back/DB/mysql.js
@@ -1,19 +1,17 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  port: 3308,
-  password: "sad123",
-  database: "savebook"
-})
-
-conn.connect((err) => {
-  if(err) {
+const database = async() => {
+  try {
+    let conn = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      port: '3308',
+      password: 'sad123',
+      database: 'savebook'
+    });
+  } catch (err) {
     console.log(err);
-  } else {
-    console.log("데이터베이스 접속")
   }
-})
+}
 
-module.exports = conn;
+module.exports = database;
--- pc-back/app.js
+++ pc-back/app.js
@@ -1,17 +1,16 @@
-const conn = require("./DB/mysql");
+const db = require("./DB/mysql");
 const express = require('express');
 const app = express();
 const PORT = 8080;
 
+db();
 app.set('view engine', 'html');
-
 app.use(express.static(__dirname + '../../pc-front/pc-react/build'));
 console.log(__dirname)
 
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
-
 app.listen(PORT, () => {
   console.log(`http:localhost:${PORT}`);
 })

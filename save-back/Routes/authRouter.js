66dd130cf4dcdc039fa69cc2f9e5c4f87ed2a41f
Kimseonghyoek <cilo1223@naver.com>
2022-09-25 04:56:28 +0900

    Deleted Log

--- save-back/Routes/authRouter.js
+++ save-back/Routes/authRouter.js
@@ -7,14 +7,12 @@ const getConnect  = require("../DB/mysql");
 router.post(
   "/post",
   (req, res, next) => {
-    console.log("얘도?");
     passport.authenticate("local", {
       session: false,
     })(req, res, next);
   },
   (req, res) => {
     if (req.user) {
-      console.log("탐1?");
       const user = { email: req.user.email, nickname: req.user.nickname };
       res.send(user);
     }

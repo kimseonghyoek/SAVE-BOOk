f289ec8cd4ca1b4bfd0e2ffc077f07f204875df4
Kimseonghyoek <cilo1223@naver.com>
2022-10-02 04:35:11 +0900

    Update Main Page

--- save-front/src/page/Main.js
+++ save-front/src/page/Main.js
@@ -10,24 +10,32 @@ const Main = () => {
       <PC>
         {/* MainContent */}
         <div className="flex flex-col items-center static">
-          <div
-            id="mainC"
-            className="w-mainC h-mainC flex items-center justify-between m-2 rounded-half"
-          >
-            <div className="w-hw flex justify-center">
-              <img
-                src={process.env.PUBLIC_URL + "/img/mainC.png"}
-                width="80%"
-                className="h-hw"
-              />
+          <div className="flex justify-center w-full2 bg-test">
+            <div
+              id="mainC"
+              className="w-mainC h-mainC flex items-center justify-between m-2 rounded-half"
+            >
+              <div className="w-hw flex justify-center">
+                <img
+                  src={process.env.PUBLIC_URL + "/img/mainC.png"}
+                  width="80%"
+                  className="h-hw"
+                />
+              </div>
+              <div className="w-hw flex items-center flex-col text-bold text-white">
+                <h1>2022년 드라마 최고작</h1>
+                <h1 className="font-Gamja text-big2">이상한 변호사 우영우</h1>
+                <h1>1, 2권 세트 판매 중</h1>
+              </div>
             </div>
-            <div className="w-hw flex items-center flex-col text-bold text-white">
-              <h1>2022년 드라마 최고작</h1>
-              <h1 className="font-Gamja text-big2">이상한 변호사 우영우</h1>
-              <h1>1, 2권 세트 판매 중</h1>
+            <div className="bg-blue m-2 w-half text-middle rounded-half text-white p-8 flex flex-col items-center justify-between">
+              <p>'45만' 코딩 유튜버 조코딩</p>
+              <p>그의 첫 도서</p>
+              <img src={process.env.PUBLIC_URL + "/img/ala.jpg"} width="100%" className="pt-16 pb-16"/>
+              <p>현재 판매 중!</p>
             </div>
           </div>
-
+          adsfadfadsfasdfadfasdf
         </div>
       </PC>
     </>
--- save-front/tailwind.config.js
+++ save-front/tailwind.config.js
@@ -13,7 +13,9 @@ module.exports = {
       "green" : "#00B388",
       "green2" : "#00916e", 
       "popup" : "#616672",
-      "danger" : "#CD3861" 
+      "danger" : "#CD3861",
+      "test" : "#BAFFCD",
+      "blue" : "#6198FF"
     },
     width: {
       "wrap" : "1000px",
@@ -30,7 +32,7 @@ module.exports = {
       "Mprofile" : "50px",
       "itemsbtn" : "28px",
       "full2" : "100%",
-      "mainC" : "85%"
+      "mainC" : "75%"
     },
     height: {
       "square" : "20px",
@@ -66,10 +68,10 @@ module.exports = {
       "small2" : "12px",
       "small":"8px",
       "big":"100px",
-      "big2" : "85px",
+      "big2" : "75px",
       "middle": "25px",
       "middle2": "20px",
-      "bold" : "45px"
+      "bold" : "35px"
     },
     backgroundImage: {
       'home' : `url("data:image/svg+xml,%3Csvg id='visual' viewBox='0 0 900 600' width='900' height='600' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1'%3E%3Crect x='0' y='0' width='900' height='600' fill='%23001220'%3E%3C/rect%3E%3Cdefs%3E%3ClinearGradient id='grad1_0' x1='33.3%25' y1='100%25' x2='100%25' y2='0%25'%3E%3Cstop offset='20%25' stop-color='%2390b77d' stop-opacity='1'%3E%3C/stop%3E%3Cstop offset='80%25' stop-color='%2390b77d' stop-opacity='1'%3E%3C/stop%3E%3C/linearGradient%3E%3C/defs%3E%3Cdefs%3E%3ClinearGradient id='grad1_1' x1='33.3%25' y1='100%25' x2='100%25' y2='0%25'%3E%3Cstop offset='20%25' stop-color='%2390b77d' stop-opacity='1'%3E%3C/stop%3E%3Cstop offset='80%25' stop-color='%23001220' stop-opacity='1'%3E%3C/stop%3E%3C/linearGradient%3E%3C/defs%3E%3Cdefs%3E%3ClinearGradient id='grad2_0' x1='0%25' y1='100%25' x2='66.7%25' y2='0%25'%3E%3Cstop offset='20%25' stop-color='%2390b77d' stop-opacity='1'%3E%3C/stop%3E%3Cstop offset='80%25' stop-color='%2390b77d' stop-opacity='1'%3E%3C/stop%3E%3C/linearGradient%3E%3C/defs%3E%3Cdefs%3E%3ClinearGradient id='grad2_1' x1='0%25' y1='100%25' x2='66.7%25' y2='0%25'%3E%3Cstop offset='20%25' stop-color='%23001220' stop-opacity='1'%3E%3C/stop%3E%3Cstop offset='80%25' stop-color='%2390b77d' stop-opacity='1'%3E%3C/stop%3E%3C/linearGradient%3E%3C/defs%3E%3Cg transform='translate(900, 600)'%3E%3Cpath d='M-337 0C-329 -41.1 -321 -82.3 -309.5 -128.2C-298 -174.1 -282.8 -224.8 -248.6 -248.6C-214.3 -272.4 -161 -269.3 -116.3 -280.9C-71.7 -292.4 -35.9 -318.7 0 -345L0 0Z' fill='%23126264'%3E%3C/path%3E%3Cpath d='M-168.5 0C-164.5 -20.6 -160.5 -41.1 -154.7 -64.1C-149 -87.1 -141.4 -112.4 -124.3 -124.3C-107.2 -136.2 -80.5 -134.6 -58.2 -140.4C-35.9 -146.2 -17.9 -159.4 0 -172.5L0 0Z' fill='%2390b77d'%3E%3C/path%3E%3C/g%3E%3Cg transform='translate(0, 0)'%3E%3Cpath d='M343 0C333.1 44.8 323.1 89.5 303 125.5C282.9 161.5 252.7 188.7 224.9 224.9C197 261.1 171.6 306.3 134.5 324.8C97.5 343.3 48.7 335.2 0 327L0 0Z' fill='%23126264'%3E%3C/path%3E%3Cpath d='M171.5 0C166.5 22.4 161.6 44.8 151.5 62.8C141.5 80.7 126.3 94.3 112.4 112.4C98.5 130.5 85.8 153.1 67.3 162.4C48.7 171.7 24.4 167.6 0 163.5L0 0Z' fill='%2390b77d'%3E%3C/path%3E%3C/g%3E%3C/svg%3E")`,

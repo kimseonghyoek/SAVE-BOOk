0dc8a0cbe16f092b52eb8104e0f8aeec32178fb1
Kimseonghyoek <cilo1223@naver.com>
2022-10-05 06:06:00 +0900

    Finished BestSeller Crawling!~ :)

--- save-crawling/bestseller.py
+++ save-crawling/bestseller.py
@@ -22,21 +22,29 @@ book_contents = book_list.findAll("div", {"class" :  "detail"})
 title = [i.find("div", {"class" : "title"}).find("strong").text for i in book_contents]
 # 저자
 author = [i.find("div", {"class" : "author"}).text[24 : 26] for i in book_contents]
+# 이미지
+cover = book_list.findAll("div", {"class" : "cover"})
+book_img = [i.find("img") for i in cover]
 
 # BestSeller.json 파일 생성
-# a = []
-# with open('BestSeller.json', 'a+', encoding='UTF-8') as make_file:
-#   for i in range(len(title)) :
-#     text = []
-#     file_data["title"] = title[i]
-#     file_data["author"] = author[i]
-#     text = json.loads(json.dumps(file_data, ensure_ascii=False))
-#     a.append(text)
-#   json.dump(a, make_file, ensure_ascii=False)
+a = []
+with open('BestSeller.json', 'a+', encoding='UTF-8') as make_file:
+  for i in range(len(title)) :
+    text = []
+    file_data["title"] = title[i]
+    file_data["author"] = author[i]
+    text = json.loads(json.dumps(file_data, ensure_ascii=False))
+    a.append(text)
+  json.dump(a, make_file, ensure_ascii=False)
 
 
 # 이미지 가져오기
-img = book_list.find("img")["src"]
-print(img)
-urllib.request.urlretrieve(img, str(os.getcwd())+'/save-crawling'+'/test'+'.jpg')
-print(os.getcwd())
+img_list = []
+file_num = 0
+print(book_img)
+for i in book_img :
+  img_src1 = i["src"]
+  print(img_src1)
+  img_list.append(img_src1)
+  urllib.request.urlretrieve(img_list[file_num], str(os.getcwd())+'/img/'+str(file_num)+'.jpg')
+  file_num += 1

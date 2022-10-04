802833b7df9ae666ceb9333d80bd479c2c222243
Kimseonghyoek <cilo1223@naver.com>
2022-10-05 04:40:02 +0900

    Crawling BestSeller Book Img

--- save-crawling/bestseller.py
+++ save-crawling/bestseller.py
@@ -1,8 +1,12 @@
 # 교보문고 베스트 셀러 크롤링
+from re import I
 from typing import OrderedDict
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import json
+import urllib.request
+import urllib
+import os
 
 from matplotlib.pyplot import text
 from numpy import append
@@ -18,12 +22,21 @@ book_contents = book_list.findAll("div", {"class" :  "detail"})
 title = [i.find("div", {"class" : "title"}).find("strong").text for i in book_contents]
 # 저자
 author = [i.find("div", {"class" : "author"}).text[24 : 26] for i in book_contents]
-a = []
-with open('BestSeller.json', 'a+', encoding='UTF-8') as make_file:
-  for i in range(len(title)) :
-    text = []
-    file_data["title"] = title[i]
-    file_data["author"] = author[i]
-    text = json.loads(json.dumps(file_data, ensure_ascii=False))
-    a.append(text)
-  json.dump(a, make_file, ensure_ascii=False)
+
+# BestSeller.json 파일 생성
+# a = []
+# with open('BestSeller.json', 'a+', encoding='UTF-8') as make_file:
+#   for i in range(len(title)) :
+#     text = []
+#     file_data["title"] = title[i]
+#     file_data["author"] = author[i]
+#     text = json.loads(json.dumps(file_data, ensure_ascii=False))
+#     a.append(text)
+#   json.dump(a, make_file, ensure_ascii=False)
+
+
+# 이미지 가져오기
+img = book_list.find("img")["src"]
+print(img)
+urllib.request.urlretrieve(img, str(os.getcwd())+'/save-crawling'+'/test'+'.jpg')
+print(os.getcwd())

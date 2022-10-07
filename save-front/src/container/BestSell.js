f9b5444cd6a8aa40aa691f1dde99f12d07df9f45
Kimseonghyoek <cilo1223@naver.com>
2022-10-08 01:01:04 +0900

    Update BestSell UI

--- save-front/src/container/BestSell.js
+++ save-front/src/container/BestSell.js
@@ -2,7 +2,7 @@ const BestSell = ({ children }) => {
   return (
     <>
       <h1 className="text-middle pt-14">베스트셀러 * BestSeller</h1>
-      <div className="p-8 flex flex-col items-center">
+      <div className="p-4 flex flex-col items-center">
         <ul>{children}</ul>
       </div>
     </>
